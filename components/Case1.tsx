8bdd4f71f558e6d7fe44976651f255cbf87956c1
chiarajugno <jugnochiara@gmail.com>
2024-12-03 20:25:26 +0800

    added run test button

--- components/Case1.tsx
+++ components/Case1.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Roboto } from 'next/font/google';
+import { Button } from '@mantine/core';
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -9,11 +10,35 @@ const roboto = Roboto({
 
 export default function Case1() {
     return (
-      <div className='flex flex-col items-start m-20'>
-        <h1 className='text-[24px]' style={roboto.style}>
-          <span className='font-semibold'>Case #1: </span> 
-          Concurrent transactions in two or more nodes are reading the same data item.
-        </h1>
+      <div className='flex flex-col items-center justify-start mx-12 my-20'>
+        <div className='flex flex-col mb-8'>
+          <h1 className='text-[24px]' style={roboto.style}>
+            <span className='font-semibold'>Case #1: </span> 
+            Concurrent transactions in two or more nodes are reading the same data item.
+          </h1>
+        </div>
+        <div className='flex flex-row w-full justify-between mb-8'>
+          <div className='flex flex-col w-[30%] h-[350px] p-4 items-start justify-start border-2 border-black rounded-md'>
+            <p className="text-[20px] font-semibold" style={roboto.style}>Node 1</p>
+            <p className="text-[18px] text-[#4682B4]" style={roboto.style}>
+              SELECT * FROM all_games WHERE app_id = 1;
+            </p>
+
+          </div>
+          <div className='flex flex-col w-[30%] h-[350px] p-4 items-start justify-start border-2 border-black rounded-md'>
+            <p className="text-[20px] font-semibold" style={roboto.style}>Node 2</p>
+            <p className="text-[18px] text-[#4682B4]" style={roboto.style}>
+              SELECT * FROM before_2020 WHERE app_id = 1;
+            </p>
+          </div>
+          <div className='flex flex-col w-[30%] h-[350px] p-4 items-start justify-start border-2 border-black rounded-md'>
+            <p className="text-[20px] font-semibold" style={roboto.style}>Node 3</p>
+            <p className="text-[18px] text-[#4682B4]" style={roboto.style}>
+              SELECT * FROM after_and_2020 WHERE app_id = 1;
+            </p>
+          </div>
+        </div>
+        <Button variant="filled">Run Test</Button>
       </div>
         
     );
