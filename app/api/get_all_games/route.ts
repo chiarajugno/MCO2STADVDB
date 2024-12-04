7b77e90d411e60d88e3deb781a945aa5b1526b19
chiarajugno <jugnochiara@gmail.com>
2024-12-04 22:36:41 +0800

    added search functionality

--- app/api/get_all_games/route.ts
+++ app/api/get_all_games/route.ts
@@ -8,10 +8,11 @@ export async function GET(request: Request) {
     const page = parseInt(url.searchParams.get('page') || '1', 10);
     const limit = parseInt(url.searchParams.get('limit') || '20', 10);
     const offset = (page - 1) * limit;
+    const search = url.searchParams.get('search') || '';
 
     try {
         const db = await createConnectionCentral();
-        
+
         if (db == null) {
             console.log("CONNECTION FAILED, TRYING NODE 2");
             const db = await createConnection2();
@@ -23,11 +24,17 @@ export async function GET(request: Request) {
                 }
             }
         } else {
-            const query = `SELECT * FROM all_games LIMIT ? OFFSET ?`;
-            const [games] = await db.query(query, [limit, offset]);
+            const query = `
+                SELECT * FROM all_games
+                WHERE name LIKE ?
+                LIMIT ? OFFSET ?`;
+            const [games] = await db.query(query, [`%${search}%`, limit, offset]);
 
-            const countQuery = `SELECT COUNT(*) as total FROM all_games`;
-            const total = await db.query(countQuery);
+            const countQuery = `
+                SELECT COUNT(*) as total
+                FROM all_games
+                WHERE name LIKE ?`;
+            const total = await db.query(countQuery, [`%${search}%`]);
 
             return NextResponse.json({ games, total });
         }
--- components/Node.tsx
+++ components/Node.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Game } from '@/types/types';
-import { MantineProvider, Table, Pagination, Loader, UnstyledButton, Drawer, Button } from '@mantine/core';
+import { MantineProvider, Table, Pagination, Loader, UnstyledButton, Drawer, Button, TextInput } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { Roboto } from 'next/font/google';
 
@@ -16,14 +16,14 @@ type Props = {
 }
 
 
-export default function Node({ node } : Props) {
+export default function Node({ node }: Props) {
     const [fetchedGames, setFetchedGames] = useState<Game[]>([]);
     const [totalGames, setTotalGames] = useState(0);
     const [currentPage, setCurrentPage] = useState(1);
     const [loading, setLoading] = useState(true);
-    const limit = 20; 
+    const [searchQuery, setSearchQuery] = useState('');
+    const limit = 20;
     const [opened, { open, close }] = useDisclosure(false);
-
 
     const fetchGames = async (page: number) => {
         let route = "";
@@ -38,7 +38,7 @@ export default function Node({ node } : Props) {
         }
         try {
             console.log("NODE: ", node);
-            const data = await fetch(`/api/${route}?page=${page}&limit=${limit}`);
+            const data = await fetch(`/api/${route}?page=${page}&limit=${limit}&search=${encodeURIComponent(searchQuery)}`);
             const { games, total } = await data.json();
             console.log(games, total);
 
@@ -54,14 +54,14 @@ export default function Node({ node } : Props) {
 
     useEffect(() => {
         fetchGames(currentPage);
-    }, [currentPage]);
+    }, [currentPage, searchQuery]);
 
     const totalPages = Math.ceil(totalGames / limit);
 
     return (
         <MantineProvider>
             <div className="w-full flex flex-col items-center justify-center p-20">
-                <Drawer size="xs" opened={opened} onClose={close}>
+            <Drawer size="xs" opened={opened} onClose={close}>
                     <div className='flex flex-col items-start justify-center px-4'>
                         <p className="text-[28px] font-bold mb-4" style={roboto.style}>
                             CRUD Menu
@@ -84,38 +84,49 @@ export default function Node({ node } : Props) {
                     </div>
                 </Drawer>
 
-                <Button className="mr-auto mb-8" onClick={open}>CRUD Menu</Button>
-                <div className="flex flex-row justify-center space-x-4 w-full">
-                    <UnstyledButton component="a" href={`/case1/${node}`}>
-                        <div className="flex flex-col items-center justify-center w-[164px] h-[40px] pt-1 mb-10
-                                        hover:ease-in hover:duration-200 hover:bg-[#23012C]
-                                        bg-[#531A88] shadow-xl rounded-lg">
-                            <p className="text-[16px] text-[#FFFFFF]">
-                                Case 1
-                            </p>
-                        </div>
-                    </UnstyledButton>
+                <div className='flex flex-row w-full items-center justify-between mb-8'>
+                    <Button onClick={open}>CRUD Menu</Button>
+                    <div className="flex flex-row justify-center space-x-4">
+                        <UnstyledButton component="a" href={`/case1/${node}`}>
+                            <div className="flex flex-col items-center justify-center w-[164px] h-[40px] pt-1
+                                            hover:ease-in hover:duration-200 hover:bg-[#23012C]
+                                            bg-[#531A88] shadow-xl rounded-lg">
+                                <p className="text-[16px] text-[#FFFFFF]">
+                                    Case 1
+                                </p>
+                            </div>
+                        </UnstyledButton>
 
-                    <UnstyledButton component="a" href={`/case2/${node}`}>
-                        <div className="flex flex-col items-center justify-center w-[164px] h-[40px] pt-1 mb-10
-                                        hover:ease-in hover:duration-200 hover:bg-[#23012C]
-                                        bg-[#531A88] shadow-xl rounded-lg">
-                            <p className="text-[16px] text-[#FFFFFF]">
-                                Case 2
-                            </p>
-                        </div>
-                    </UnstyledButton>
+                        <UnstyledButton component="a" href={`/case2/${node}`}>
+                            <div className="flex flex-col items-center justify-center w-[164px] h-[40px] pt-1
+                                            hover:ease-in hover:duration-200 hover:bg-[#23012C]
+                                            bg-[#531A88] shadow-xl rounded-lg">
+                                <p className="text-[16px] text-[#FFFFFF]">
+                                    Case 2
+                                </p>
+                            </div>
+                        </UnstyledButton>
 
-                    <UnstyledButton component="a" href={`/case3/${node}`}>
-                        <div className="flex flex-col items-center justify-center w-[164px] h-[40px] pt-1 mb-10
-                                        hover:ease-in hover:duration-200 hover:bg-[#23012C]
-                                        bg-[#531A88] shadow-xl rounded-lg">
-                            <p className="text-[16px] text-[#FFFFFF]">
-                                Case 3
-                            </p>
-                        </div>
-                    </UnstyledButton>
+                        <UnstyledButton component="a" href={`/case3/${node}`}>
+                            <div className="flex flex-col items-center justify-center w-[164px] h-[40px] pt-1
+                                            hover:ease-in hover:duration-200 hover:bg-[#23012C]
+                                            bg-[#531A88] shadow-xl rounded-lg">
+                                <p className="text-[16px] text-[#FFFFFF]">
+                                    Case 3
+                                </p>
+                            </div>
+                        </UnstyledButton>
+                    </div>
+                    <TextInput
+                        placeholder="Search games..."
+                        value={searchQuery}
+                        onChange={(e) => {
+                            setSearchQuery(e.target.value);
+                            setCurrentPage(1); 
+                        }}
+                    />
                 </div>
+                
 
                 
                 <h1 className="text-[32px] font-semibold mb-4" style={roboto.style}>
